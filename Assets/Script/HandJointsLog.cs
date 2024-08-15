03298cfe8bbb8846815e8d6ccf77289c77f4892d
winston-nguyenn <qtruongng@gmail.com>
2024-08-15 12:28:24 -0400

    successfully save all joints to cloud

--- Assets/Script/HandJointsLog.cs
+++ Assets/Script/HandJointsLog.cs
@@ -12,13 +12,13 @@ using Unity.Services.CloudSave;
 using Unity.Services.CloudSave.Models;
 using Unity.Services.Core;
 
- public class LogBoneLocation : MonoBehaviour
+ public class HandJointsLog : MonoBehaviour
  {
-     [SerializeField]
-     private Hand hand;
-     private Pose currentPose;
-     private HandJointId handJointId = HandJointId.HandIndex3; // TO DO: Change this to your bone.
-     private List<string> poseDataList = new List<string>();
+    [SerializeField]
+    private Hand hand;
+    private Pose currentPose;
+    private List<HandJointId> handJointIds;
+    private List<string> poseDataList = new List<string>();
     private DateTime startTime;
     private TimeSpan uploadInterval = TimeSpan.FromSeconds(30); // Interval in seconds
     // Start is called before the first frame update
@@ -27,19 +27,26 @@ using Unity.Services.Core;
         // Sign in and Initialize Cloud service
         await UnityServices.InitializeAsync();
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        // Initialize the list of hand joints
+        handJointIds = new List<HandJointId>((HandJointId[])System.Enum.GetValues(typeof(HandJointId)));
+
         // Add header
-        poseDataList.Add("Timestamp (yyyyMMdd_HHmmss),PositionX,PositionY,PositionZ,RotationX,RotationY,RotationZ,RotationW");
+        poseDataList.Add("Timestamp (yyyyMMdd_HHmmss.ffff),Joint,PositionX,PositionY,PositionZ,RotationX,RotationY,RotationZ,RotationW");
+
         // Initialize Start time
         startTime = DateTime.Now;
     }
 
     void Update()
     {
-        // Check if hand pose is detected. If so, save it.
-        if (hand.GetJointPose(handJointId, out currentPose))
+        // Loop through all joints: if the hand joint is detected, save tracking data of the joint to a string
+        foreach (HandJointId jointId in handJointIds)
         {
-            SavePoseToString(currentPose);
-            SavePoseToCloud(currentPose);
+            if (hand.GetJointPose(jointId, out currentPose))
+            {
+                SavePoseToString(jointId, currentPose);
+            }
         }
         // Check if the interval has passed to save a csv file to Unity Cloud
         if (DateTime.Now - startTime >= uploadInterval)
@@ -49,30 +56,31 @@ using Unity.Services.Core;
         }
     }
 
-    void SavePoseToString(Pose pose)
+    void SavePoseToString(HandJointId jointId, Pose pose)
     {
         string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss.ffff");
         // Position in meters and Rotation in quaternions (To convert a quaternion to Euler angles, use the Quaternion.eulerAngles function)
-        string poseData = $"{timestamp},{pose.position.x},{pose.position.y},{pose.position.z}," +
+        string poseData = $"{timestamp},{jointId},{pose.position.x},{pose.position.y},{pose.position.z}," +
                               $"{pose.rotation.x},{pose.rotation.y},{pose.rotation.z},{pose.rotation.w}"; 
         poseDataList.Add(poseData);
     }
     private async void SaveCSVToCloud()
     {
+        // File name format
         string endTimeStamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
-        string fileName = $"HandIndex3_{endTimeStamp}.csv";
-        // string filePath = Path.Combine(Application.persistentDataPath, fileName);
-        string csvData = string.Join("\n", poseDataList);
-        // File.WriteAllText(filePath, csvData);
+        string fileName = $"HandJoints_{endTimeStamp}.csv";
 
-        //byte[] fileData = File.ReadAllBytes(filePath);
+        // Convert all hand tracking data to a string
+        string csvData = string.Join("\n", poseDataList);
         byte[] fileData = Encoding.UTF8.GetBytes(csvData);
+
+        // Save hand tracking data to Unity Cloud
         try
         {
             await CloudSaveService.Instance.Files.Player.SaveAsync(fileName, fileData);
             Debug.Log("CSV uploaded to cloud successfully.");
         }
-       catch (CloudSaveValidationException e)
+        catch (CloudSaveValidationException e)
         {
             Debug.LogError(e);
         }
